444f6c8becf6675931086e997a9bf4c104f037c2
Javier <javier.data.scientist@outlook.com>
2024-01-11 18:33:06 +0100

    feat: improve sitemap.js

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,49 +1,17 @@
-// import { getSortedPostsData } from "../lib/posts";
-import { getAllArticles, getArticleSlugs } from "@/lib/articles/parsers";
+import { getArticleSlugs } from "@/lib/articles/parsers";
 
 const URL = "https://javiiccode.com";
 
 export default async function sitemap() {
-  //   const posts = await getAllArticles();
-  //   console.log(`POSTS ------- ${posts}`);
   const slugs = await getArticleSlugs();
-  //   console.log(`SLUGS ------- ${slugs}`);
-  //   console.log(slugs);
-  //   console.log(slugs[0]);
-  //   console.log(typeof slugs[0]);
-  //   console.log(`SLUGS ------- ${typeof slugs}`);
 
-  // let posts = {}
-  // if (!slugs) {
-  //   posts = {}
-  // } else if (posts.length === 0) {
-  //   posts = {}
-  // } else {
-  //   posts = slugs.map((slug) => ({
-  //     url: `${URL}/blog/${slug}`,
-  //   }));
-  // }
-
-  // if (slugs.length === 0) {
-  //   const posts = {};
-  // } else {
-  //   const posts = slugs.map((slug) => ({
-  //     url: `${URL}/blog/${slug}`,
-  //   }));
-  // }
   const posts = slugs.map((slug) => ({
     url: `${URL}/blog/${slug}`,
   }));
 
-  // lastModified: date,
-  //   console.log("--------------------------");
-  //   console.log(posts);
-  //   console.log("--------------------------");
-
-  const routes = ["", "/blog", "/contact"].map((route) => ({
+  const routes = ["", "/blog"].map((route) => ({
     url: `${URL}${route}`,
   }));
-  // lastModified: new Date().toISOString(),
 
   return [...routes, ...posts];
 }
